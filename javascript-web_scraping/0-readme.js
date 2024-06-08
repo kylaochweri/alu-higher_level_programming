e1013732a2843942cd0cebe70c600bf61ee1a3a9
kylaochweri <k.ochweri@alustudent.com>
2024-06-08 21:26:34 +0300

    complete

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,20 +1,6 @@
 #!/usr/bin/node
 const fs = require('fs');
-
-function readFile (filePath) {
-  fs.readFile(filePath, 'utf-8', (err, data) => {
-    if (err) {
-      console.error(`An error occurred while reading the file: ${err}`);
-    } else {
-      console.log(data);
-    }
-  });
-}
-
-// Check if the file path is provided as an argument
-if (process.argv.length > 2) {
-  const filePath = process.argv[2];
-  readFile(filePath);
-} else {
-  console.log('Please provide the file path as an argument.');
-}
+fs.readFile(process.argv[2], (error, paragraph) => {
+  if (error) throw error;
+  console.log(paragraph.toString());
+});
