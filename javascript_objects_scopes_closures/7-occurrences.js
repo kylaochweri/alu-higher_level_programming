3b0d30d03ecc5e62d7391bd46338a742ac53b28d
kylaochweri <k.ochweri@alustudent.com>
2024-06-08 20:24:38 +0300

    learnt java

--- javascript_objects_scopes_closures/7-occurrences.js
+++ javascript_objects_scopes_closures/7-occurrences.js
@@ -1,4 +1,11 @@
 #!/usr/bin/node
+
 exports.nbOccurences = function (list, searchElement) {
-  return list.reduce((count, current) => current === searchElement ? count + 1 : count, 0);
+  let nOccurences = 0;
+  for (let i = 0; i < list.length; i++) {
+    if (searchElement === list[i]) {
+      nOccurences++;
+    }
+  }
+  return nOccurences;
 };
